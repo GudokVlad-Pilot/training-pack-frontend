016d4404712fc31c4e6c340d089fcec2d492d4da
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2026-05-03 23:13:54 +0300

    feat(page): added finance page

---
+++ src/app/homeContent.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+
+export default function HomeContent() {
+
+  const router = useRouter()
+
+  const navigateToFinance = () => router.push("/finances")
+
+  return (
+    <div className={styles.page}>
+      Hello world!
+      <div onClick={navigateToFinance}>
+        Finances
+      </div>
+    </div>
+  );
+}
