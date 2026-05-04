a352eb51f00c0c351f54dbcc4f7171760256001b
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2026-05-04 20:50:56 +0300

    feat(nav): added basic navigation to home

--- src/app/finances/financesContent.tsx
+++ src/app/finances/financesContent.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function FinancesContent() {
-  return <div>Finances</div>;
+  const router = useRouter();
+
+  const navigateToHome = () => router.replace("/"); // Replace in terms of disallowing to navigate back from home page
+
+  return (
+    <div>
+      <div>Finances</div>
+      <div onClick={navigateToHome}>Home</div>
+    </div>
+  );
 }
